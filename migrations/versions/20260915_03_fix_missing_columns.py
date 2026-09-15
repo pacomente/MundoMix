173322d7bca6e4333b22e29cffd27d9b9a84fffd
Emanuel <91442685+pacomente@users.noreply.github.com>
2026-09-15 17:53:21 -0300

    Update 20260915_03_fix_missing_columns.py

--- migrations/versions/20260915_03_fix_missing_columns.py
+++ migrations/versions/20260915_03_fix_missing_columns.py
@@ -9,7 +9,7 @@ import sqlalchemy as sa
 # revision identifiers, used by Alembic.
 # Ejemplo:
 revision = 'a1b2c3d4'
-down_revision = None  # <-- Asegurate de que este ID coincida con el 'revision' de la migración anterior existente
+down_revision = "fe892bc0"  # <-- Asegurate de que este ID coincida con el 'revision' de la migración anterior existente
 branch_labels = None
 depends_on = None
 
