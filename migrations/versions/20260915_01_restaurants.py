0ba02c56bb1e46fe873a71c10c5db6a23a6598d1
Emanuel <91442685+pacomente@users.noreply.github.com>
2026-09-15 15:58:09 -0300

    Update 20260915_01_restaurants.py

--- migrations/versions/20260915_01_restaurants.py
+++ migrations/versions/20260915_01_restaurants.py
@@ -3,7 +3,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = "20260915_01_restaurants.py"
-down_revision = None
+down_revision = "20260915_02_gastronomia_20"
 branch_labels = None
 depends_on = None
 
