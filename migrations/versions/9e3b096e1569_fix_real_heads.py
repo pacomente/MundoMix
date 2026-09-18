1c29c64ad12e5d9cd9c6e578808878cd150c053b
Emanuel <91442685+pacomente@users.noreply.github.com>
2026-09-18 16:18:24 -0300

    Add files via upload

---
+++ migrations/versions/9e3b096e1569_fix_real_heads.py
@@ -0,0 +1,14 @@
+"""fix real heads"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = '9e3b096e1569'
+down_revision = '20260918_04_cloudinary'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
