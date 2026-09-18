055d2cff8eb0e5aaf4c5dd18de0ac95504837eaa
Emanuel <91442685+pacomente@users.noreply.github.com>
2026-09-18 16:26:05 -0300

    Update 9e3b096e1569_fix_real_heads.py

--- migrations/versions/9e3b096e1569_fix_real_heads.py
+++ migrations/versions/9e3b096e1569_fix_real_heads.py
@@ -3,7 +3,7 @@ from alembic import op
 import sqlalchemy as sa
 
 revision = '999999999999'
-down_revision = ('20260918_05_schema_indexes', 'OTRO_CODIGO')
+down_revision = ('20260918_05_schema_indexes', '20260918_04_cloudinary')
 branch_labels = None
 depends_on = None
 
