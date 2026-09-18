c14de929de0e74057737a5a600139c2dd74c47d8
Emanuel <91442685+pacomente@users.noreply.github.com>
2026-09-18 16:02:20 -0300

    Add files via upload

---
+++ migrations/versions/54a6bdeffe1f_fix_heads.py
@@ -0,0 +1,14 @@
+"""fix heads"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = '54a6bdeffe1f'
+down_revision = 'b6e73856b787'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
