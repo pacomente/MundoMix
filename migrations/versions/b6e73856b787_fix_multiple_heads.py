7e2a20777458391e40f0d80f4428637ee1a56a73
Emanuel <91442685+pacomente@users.noreply.github.com>
2026-09-18 16:08:12 -0300

    Add files via upload

---
+++ migrations/versions/b6e73856b787_fix_multiple_heads.py
@@ -0,0 +1,14 @@
+"""fix multiple heads"""
+from alembic import op
+import sqlalchemy as sa
+
+revision = 'b6e73856b787'
+down_revision = 'e86b56b88f55'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
