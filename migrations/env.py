32c8d952f9fd84eb3bb9e4963250ca66a2ab4eef
Emanuel <91442685+pacomente@users.noreply.github.com>
2026-09-19 17:14:26 -0300

    Update env.py

--- migrations/env.py
+++ migrations/env.py
@@ -19,20 +19,33 @@ def get_url():
 
 
 def run_migrations_offline():
-    context.configure(url=get_url(), target_metadata=target_metadata, literal_binds=True, dialect_opts={"paramstyle": "named"})
-    with context.begin_transaction():
-        context.run_migrations()
+    with app.app_context():
+        context.configure(
+            url=get_url(),
+            target_metadata=target_metadata,
+            literal_binds=True,
+            dialect_opts={"paramstyle": "named"},
+        )
+        with context.begin_transaction():
+            context.run_migrations()
 
 
 def run_migrations_online():
-    connectable = db.engine
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata, compare_type=True, compare_server_default=True)
-        with context.begin_transaction():
-            context.run_migrations()
+    with app.app_context():
+        connectable = db.engine
+        with connectable.connect() as connection:
+            context.configure(
+                connection=connection,
+                target_metadata=target_metadata,
+                compare_type=True,
+                compare_server_default=True,
+            )
+            with context.begin_transaction():
+                context.run_migrations()
 
 
 if context.is_offline_mode():
     run_migrations_offline()
 else:
+    run_migrations_online()
     run_migrations_online()
