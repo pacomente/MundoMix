6b75bb9ae5cae4bc21342116dafd38f634184091
Emanuel <91442685+pacomente@users.noreply.github.com>
2026-09-19 16:33:37 -0300

    Delete slugify.py

--- slugify.py
+++
@@ -1,5 +0,0 @@
-import re, unicodedata
-def make_slug(value, extra=0):
-    value = unicodedata.normalize("NFKD", value).encode("ascii","ignore").decode().lower()
-    slug = re.sub(r"[^a-z0-9]+","-",value).strip("-")
-    return f"{slug}-{extra}" if extra else slug
